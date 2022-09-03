989269fd804d59438022918e68e87e3aae8f6c9b
sifatullahsu <personal.sifat@gmail.com>
2022-09-03 10:49:18 +0600

    Resonsive & Get category Name from API

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,25 @@
-console.log(1)
+const querySelector = path =>{
+  return document.querySelector(path);
+}
+
+const newsCategoriesAPI = () => {
+  fetch('https://openapi.programming-hero.com/api/news/categories')
+  .then(response => response.json())
+  .then(data => newsCategories(data.data.news_category))
+}
+
+const newsCategories = data => {
+  const categories = querySelector('#news-category ul');
+  console.log(categories); 
+
+  data.forEach(element => {
+    const li = document.createElement('li');
+    li.innerHTML = `<button>${element.category_name}</button>`;
+    categories.append(li);
+
+    // Get Category By category_id
+    getNewsByCategoryAPI(element.category_id);
+  });
+}
+
+newsCategoriesAPI();
